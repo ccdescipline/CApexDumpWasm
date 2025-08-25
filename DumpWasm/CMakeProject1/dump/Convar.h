b1a80e8b8fe14c3571b1df4df3e5c4030d39dfeb
ccdescipline <2350549466@qq.com>
2025-08-25 18:38:01 +0800

    fix The sequence of check

--- DumpWasm/CMakeProject1/dump/Convar.h
+++ DumpWasm/CMakeProject1/dump/Convar.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "../3rd/PS.h"
 #include "../include/json.hpp"
+#include "../3rd/Log.h"
 
 class Convar {
 public:
@@ -42,6 +43,8 @@ public:
 			return false;
 		}
 
+        LogE("conVarVtable %p",conVarVtable);
+
 		//output += std::format("conVarVtable {:#016x} \r\n", conVarVtable);
 
 		conVarVtable = reinterpret_cast<std::uint64_t>( conVarVtable + ctx.baseAddress);
@@ -65,17 +68,26 @@ public:
             return false;
         }
 
+        LogE("conVarVtable matches %d",matches.size());
+
 
 		for (auto i = size_t(); i < matches.size(); i++) {
 			auto offset = matches[i];
+
 			RawConVar* convar = (RawConVar*)(ctx.data.data() + offset);
 
+            if(!PS::In(ctx.baseAddress,ctx.data.size(),convar->m_pszName,8)){
+                continue;
+            }
 
 			if (!convar->m_pszDescription || !convar->m_pszName) {
 				continue;
 			}
 
+
 			std::string name = std::string((char*)convar->m_pszName - ctx.baseAddress + (std::uint64_t)ctx.data.data());
+
+            if(!PS::isAsciiOnly(name.data())) continue;
 
 			offsets[name] = offset;
 
@@ -83,8 +95,9 @@ public:
 
 			//output += std::format("name {} value {:#x} \r\n", name.data(), (offset));
 
-			//LogE("%s %p", name.data(), offset);
+			LogE("%s %p", name.data(), offset);
 		}
+
 
         output = offsets;
 
